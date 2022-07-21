257507d7e0500598d004aa7c6a5cf5b622f19300
Niko Lipponen <niko.lipponen@devecto.com>
2022-07-21 10:59:08 +0300

    Initial Commit

---
+++ LipasApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LipasApp
+//
+//  Created by Niko Lipponen on 21.7.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
