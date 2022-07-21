cf92cf4bdf14452967251fce09a1f22e4c12a7dc
Niko Lipponen <niko.lipponen@devecto.com>
2022-07-21 12:33:26 +0300

    ApiCalls +  missing .pbxproj from previous commit

--- LipasApp/Services/LipasApiClient.swift
+++ LipasApp/Services/LipasApiClient.swift
@@ -5,8 +5,71 @@
 //  Created by Niko Lipponen on 21.7.2022.
 //
 
-import Foundation
+import Alamofire
 
 class LipasApiClient {
-    //TODO
+    private let baseUrl = "http://lipas.cc.jyu.fi/api/sports-places"
+    
+    // MARK: - getSportPlaceSimpleList
+    enum GetSportPlaceSimpleListFailureReason: Int, Error {
+        case notFound = 404
+        case emptyResponse = 460
+    }
+    
+    typealias GetSportPlaceSimpleListResult = Result<[SportPlaceSimple], GetSportPlaceSimpleListFailureReason>
+    typealias GetSportPlaceSimpleListCompletion = (_ result: GetSportPlaceSimpleListResult) -> Void
+    
+    func getSportPlaceSimpleList(completion: @escaping GetSportPlaceSimpleListCompletion) {
+        //parameters are hard because this is example app.
+        AF.request(baseUrl, parameters: ["field": "name"])
+            .validate()
+            .responseDecodable(of: [SportPlaceSimple].self) { response in
+                switch response.result {
+                case .success:
+                    do {
+                        guard let data = response.data else {
+                            completion(.failure(.emptyResponse))
+                            return
+                        }
+                        let sportPlaceSimpleList = try JSONDecoder().decode([SportPlaceSimple].self, from: data)
+                        completion(.success(sportPlaceSimpleList))
+                    } catch {
+                        completion(.failure(.notFound))
+                    }
+                case .failure:
+                    completion(.failure(.notFound))
+            }
+        }
+    }
+    
+    //MARK: - getSportPlaceDetails
+    enum GetSportPlaceDetailsFailureReason: Int, Error {
+        case notFound = 404
+        case emptyResponse = 460
+    }
+    
+    typealias GetSportPlaceDetailsResult = Result<SportPlaceDetails, GetSportPlaceDetailsFailureReason>
+    typealias GetSportPlaceDetailsCompletion = (_ result: GetSportPlaceDetailsResult) -> Void
+
+    func getSportPlaceDetails(sportPlaceId : Int, completion: @escaping GetSportPlaceDetailsCompletion) {
+        AF.request(baseUrl + "/\(sportPlaceId)")
+            .validate()
+            .responseDecodable(of: SportPlaceDetails.self) { response in
+                switch response.result {
+                case .success:
+                    do {
+                        guard let data = response.data else {
+                            completion(.failure(.emptyResponse))
+                            return
+                        }
+                        let sportPlaceDetails = try JSONDecoder().decode(SportPlaceDetails.self, from: data)
+                        completion(.success(sportPlaceDetails))
+                    } catch {
+                        completion(.failure(.notFound))
+                    }
+                case .failure:
+                    completion(.failure(.notFound))
+            }
+        }
+    }
 }
